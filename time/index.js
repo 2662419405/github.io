7683d61482d04ae62d8fc0924e4c12e7c75a3250
sh <2662419405@qq.com>
2019-12-23 14:11:04 +0800

    Site updated: 2019-12-23 14:11:03

--- time/index.js
+++ time/index.js
@@ -7,5 +7,9 @@ $(function(){
             $(this).css({"border":""}).removeClass("colour").find(".item_inner").children("p").css({"color":""})
         }
     )
-    $(".tab_inner").
+    $(".tab_inner").children("div").click(function(){
+        $this = $(this).index()
+        $(this).css({"border-color":"#191919"}).siblings().css({"border-color":"#E2E2E2"})
+        $(".timer").eq($(this).index()).css({"display":"block"}).siblings(".timer").css({"display":"none"})
+    })
 })
